04e423df11046414e56699014388800c6bb3f01a
PicoHBK <picohbk322@gmai.com>
2024-06-01 15:14:54 -0300

    ej2 terminado

--- ejercicio2/reservations.js
+++ ejercicio2/reservations.js
@@ -1,6 +1,35 @@
-class Customer {}
+class Customer {
+    constructor(id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
-class Reservation {}
+    get info() {
+        return `${this.name} - ${this.email}`;
+    }
+}
+
+class Reservation {
+    constructor(id, customer, date, guests) {
+        this.id = id;
+        this.customer = customer;
+        this.date = date;
+        this.guests = guests;
+    }
+
+    get info (){
+        return `${this.customer.info} - ${this.date.toLocaleString()} - ${this.guests}`;
+    }
+
+    static validateReservation(fecha, personas){
+        const fechaActual = new Date();
+        const fechaReserva = new Date(fecha);
+        if (personas > 0 && fechaReserva > fechaActual) return true;
+        return false;
+
+    }
+}
 
 class Restaurant {
     constructor(name) {
